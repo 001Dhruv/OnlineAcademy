e76d55f392b43f6818230d0ce2f9d19aea908336
DHRUV PATEL <119107060+001Dhruv@users.noreply.github.com>
2023-07-21 22:35:59 +0530

    login activity done

--- app/src/main/java/com/example/onlineacademy/LoginActivity.java
+++ app/src/main/java/com/example/onlineacademy/LoginActivity.java
@@ -3,12 +3,36 @@ package com.example.onlineacademy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private CheckBox togglePasswordCheckBox;
+    private EditText passwordEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        togglePasswordCheckBox = findViewById(R.id.togglePasswordCheckBox);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        togglePasswordCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                togglePasswordVisibility(isChecked);
+            }
+        });
+    }
+
+    private void togglePasswordVisibility(boolean isChecked) {
+        if (isChecked) {
+            // Show password in plain text
+            passwordEditText.setTransformationMethod(null);
+        } else {
+            // Hide password (default behavior)
+            passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
+        }
     }
 }

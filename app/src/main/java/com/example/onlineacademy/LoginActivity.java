64be26075ae77187df8227a0bebb3944594de12e
YashPatel117 <yp7112003@gmail.com>
2023-07-22 11:23:19 +0530

    fp

--- app/src/main/java/com/example/onlineacademy/LoginActivity.java
+++ app/src/main/java/com/example/onlineacademy/LoginActivity.java
@@ -2,26 +2,39 @@ package com.example.onlineacademy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     private CheckBox togglePasswordCheckBox;
     private EditText passwordEditText;
+    private TextView fp ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         togglePasswordCheckBox = findViewById(R.id.togglePasswordCheckBox);
         passwordEditText = findViewById(R.id.passwordEditText);
-
+        fp = findViewById(R.id.fp);
         togglePasswordCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 togglePasswordVisibility(isChecked);
+            }
+        });
+
+        fp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(LoginActivity.this , ForgetPasswordActivity.class);
+                startActivity(it);
             }
         });
     }
@@ -34,5 +47,6 @@ public class LoginActivity extends AppCompatActivity {
             // Hide password (default behavior)
             passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
         }
+
     }
 }

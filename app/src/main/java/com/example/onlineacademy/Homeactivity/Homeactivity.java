29022ba9f592de9f3a6c34ec117a36db0d078e1a
patelutsav7713@gmail.com <you@example.com>
2023-07-23 09:36:15 +0530

    final navbar

--- app/src/main/java/com/example/onlineacademy/Homeactivity/Homeactivity.java
+++ app/src/main/java/com/example/onlineacademy/Homeactivity/Homeactivity.java
@@ -1,19 +1,25 @@
 package com.example.onlineacademy.Homeactivity;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.onlineacademy.Homeactivity.Fragments.profile.Explore;
 import com.example.onlineacademy.Homeactivity.Fragments.profile.Home;
+import com.example.onlineacademy.Homeactivity.Fragments.profile.Live_Classes;
 import com.example.onlineacademy.Homeactivity.Fragments.profile.profile;
 import com.example.onlineacademy.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Homeactivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     BottomNavigationView bottomNavigationView;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,8 @@ public class Homeactivity extends AppCompatActivity implements BottomNavigationV
 
     final Fragment home = new Home();
     final Fragment profile = new profile();
+    final Fragment explore= new Explore();
+    final Fragment live_class= new Live_Classes();
 
 
     @Override
@@ -41,7 +49,19 @@ public class Homeactivity extends AppCompatActivity implements BottomNavigationV
                         .commit();
                 return true;
 
-            case R.id.navigation_Profile:
+                case R.id.navigation_explore:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, explore)
+                        .commit();
+                return true;
+            case R.id.navigation_Live:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, live_class)
+                        .commit();
+                return true;
+                case R.id.navigation_Profile:
                 getSupportFragmentManager()
                         .beginTransaction()
                         .replace(R.id.flFragment, profile)
